b220e75f523e4cf5c47b11a0aa77fffd456aedca
Ivan Meler <i_ivan@windowslive.com>
2016-07-23 08:17:29 +0200

    Decrease ring delay

--- n7100.mk
+++ n7100.mk
@@ -93,7 +93,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
     mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
     ro.telephony.call_ring.multiple=false \
-    ro.telephony.call_ring.delay=3000
+    ro.telephony.call_ring.delay=2000
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
